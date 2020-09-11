03189a50b5e550d5a074e14558a3917c2db4d625
caioHatoum <caiohatoum@gmail.com>
2020-09-11 11:55:31 -0300

    commit inicial

---
+++ lib/models/user.dart
@@ -0,0 +1,5 @@
+class User {
+  final String email,password;
+
+  User({this.email,this.password});
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:app_test/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //retorna widget de auth ou home
+    return Home();
+    
+  }
+}
